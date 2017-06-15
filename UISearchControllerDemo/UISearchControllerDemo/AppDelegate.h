7e832e387f20a1a6277d24d448794873a9d2c3d3
mac <JOE@macdeMac-mini.local>
2017-06-15 10:26:54 +0800

    1.创建项目和初始化

---
+++ UISearchControllerDemo/UISearchControllerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UISearchControllerDemo
+//
+//  Created by JOE on 2017/6/15.
+//  Copyright © 2017年 ZZJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UISearchControllerDemo/UISearchControllerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UISearchControllerDemo
+//
+//  Created by JOE on 2017/6/15.
+//  Copyright © 2017年 ZZJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
